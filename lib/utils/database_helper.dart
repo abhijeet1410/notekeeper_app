1e78d210db79c8898fabb54525155d9e40a9de85
Abhijeet Mohapatra <abhijeet@Abhijeets-MacBook-Air.local>
2022-03-28 21:42:56 +0530

    CRUD Operation methods created in the helper

--- lib/utils/database_helper.dart
+++ lib/utils/database_helper.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:notekeeper_app/models/note.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -13,6 +14,13 @@ class DatabaseHelper {
       _databaseHelper = DatabaseHelper._createInstance();
     }
     return _databaseHelper!;
+  }
+
+  Future<Database?> get database async {
+    if (_database == null) {
+      _database = await initializeDatabase();
+    }
+    return _database;
   }
 
   String noteTable = 'note_table';
@@ -34,7 +42,48 @@ class DatabaseHelper {
   Future<void> _createDb(Database db, int version) async {
     await db.execute(
         'CREATE TABLE $noteTable($colId INTEGER PRIMARY KEY AUTOINCREMENT,'
-            ' $colTitle TEXT, $colDescription TEXT, $colPriority INTEGER,'
-            ' $colDate TEXT)');
+        ' $colTitle TEXT, $colDescription TEXT, $colPriority INTEGER,'
+        ' $colDate TEXT)');
+  }
+
+  Future<int> insertNoteUsingRawQuery(Note note) async {
+    Database? db = await database;
+    var result = await db!.rawInsert(
+        'INSERT INTO $noteTable ($colId, $colTitle, $colDescription, $colDate, $colPriority) VALUES (${note.id}, ${note.title}, ${note.description}, ${note.date}, ${note.priority})');
+    return result;
+  }
+
+  Future<int> insertNote(Note note) async {
+    Database? db = await database;
+    var result = await db!.insert(noteTable, note.toMap());
+    return result;
+  }
+
+  Future<int> updateNote(Note note) async {
+    Database? db = await database;
+    var result = await db!.update(noteTable, note.toMap(),
+        where: '$colId = ?', whereArgs: [note.id]);
+    return result;
+  }
+
+  Future<int> updateNoteUsingRawQuery(Note note) async {
+    Database? db = await database;
+    var result = await db!.rawUpdate(
+        'UPDATE $noteTable SET $colTitle = ${note.title}, $colDescription = ${note.description} WHERE $colId = ${note.id}');
+    return result;
+  }
+
+  Future<int> deleteNote(Note note) async {
+    Database? db = await database;
+    var result =
+        await db!.delete(noteTable, where: '$colId = ?', whereArgs: [note.id]);
+    return result;
+  }
+
+  Future<int> deleteNoteUsingRawQuery(Note note) async {
+    Database? db = await database;
+    var result =
+        await db!.rawDelete('DELETE FROM $noteTable WHERE $colId = ${note.id}');
+    return result;
   }
 }
